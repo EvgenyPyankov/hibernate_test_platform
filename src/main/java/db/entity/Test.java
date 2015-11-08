2f9fdf08f21e95ae4fb6e9e6dc5c079b19dd3959
Евгений <Sleepwalker17@mail.ru>
2015-11-08 13:42:39 +0300

    initial commit

---
+++ src/main/java/db/entity/Test.java
@@ -0,0 +1,52 @@
+package db.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tests")
+public class Test {
+    private int idTest;
+    private String title;
+    private Set<User> users = new HashSet<User>();
+
+    public Test(){}
+
+    public Test(String title, Set users){
+        this.title = title;
+        this.users=users;
+    }
+
+    @Id
+    @Column(name = "id_test")
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public int getIdTest() {
+        return idTest;
+    }
+
+    public void setIdTest(int idTest) {
+        this.idTest = idTest;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @ManyToMany(cascade=CascadeType.ALL)
+    @JoinTable(name="user_pass", joinColumns=@JoinColumn(name="id_test"), inverseJoinColumns=@JoinColumn(name="id_user"))
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set users) {
+        this.users = users;
+    }
+
+
+}
---
+++ src/main/java/db/entity/User.java
@@ -0,0 +1,49 @@
+package db.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    private int idUser;
+    private String login;
+    private Set<Test> tests = new HashSet<Test>();
+
+    public User(){}
+
+    public User(String login, Set<Test> tests){
+        this.login = login;
+        this.tests=tests;
+    }
+
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @ManyToMany(mappedBy = "users")
+    public Set<Test> getTests() {
+        return tests;
+    }
+
+    public void setTests(Set tests) {
+        this.tests = tests;
+    }
+}
