d066b4eb17973a87274f8838216adb5d6dd53bc2
Евгений <Sleepwalker17@mail.ru>
2015-11-12 16:18:46 +0300

    addTest is working

--- src/main/java/db/Main.java
+++ src/main/java/db/Main.java
@@ -1,8 +1,7 @@
 package db;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import db.entity.*;
 
@@ -50,12 +49,54 @@ public class Main {
 //        System.out.println(db.getUserByEmail("king@mail.ru"));
 
 
+//        try {
+//            Test test = db.getTestById(9);
+//            if (db.getTestById(10)==null) System.out.println("Doesnt exist!");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+        Set<Question> questions;
+        Set<Answer> answers;
+        Set<Answer> answers1;
+
+        questions = new HashSet<Question>();
+        answers = new HashSet<Answer>();
+        answers1 = new HashSet<Answer>();
+
+
+
+
+
+        answers.add(new Answer(1, "yes"));
+        answers.add(new Answer(2, "rather yes"));
+        answers.add(new Answer(3, "i don't know"));
+        answers.add(new Answer(4, "rather no"));
+        answers.add(new Answer(5, "no"));
+
+        answers1.add(new Answer(1, "yesff"));
+        answers1.add(new Answer(2, "rathedfr yes"));
+        answers1.add(new Answer(3, "i dofn't know"));
+        answers1.add(new Answer(4, "rathfer no"));
+        answers1.add(new Answer(5, "nof"));
+
+
+
+        questions.add(new Question(1, "I am able to relax", answers));
+        questions.add(new Question(2, "I tend to focus on upsetting situations or events happening in my life", answers));
+        questions.add(new Question(3, "I feel fearful for no reason", answers));
+        questions.add(new Question(4, "I am as happy as the people around me", answers));
+        questions.add(new Question(5, "I have diarrhea, constipation, or other digestive problems", answers));
+        questions.add(new Question(6, "I have a dry mouth", answers1));
+        questions.add(new Question(7, "When someone snaps at me, I spend the rest of the day thinking about it", answers));
+        questions.add(new Question(8, "No matter what I do, I can't get my mind off my problems", answers));
+
         try {
-            Test test = db.getTestById(9);
-            if (db.getTestById(10)==null) System.out.println("Doesnt exist!");
+            db.addTest(new Test("new Test",TestCategory.QUESTIONAIRE,questions));
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 
 
